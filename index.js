7dd1a59c720ed4f8183a4e376c76c72381ca9b99
dohye1 <tsc03018@gmail.com>
2023-02-09 23:41:43 +0900

    sentry-cli test

---
+++ index.js
@@ -0,0 +1,36 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://bc704cfac9c4493696bce71e2f465929@o4504581152768000.ingest.sentry.io/4504638671159296",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+function foo() {
+  console.log(123);
+  throw new Error("hyewon server test");
+}
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
