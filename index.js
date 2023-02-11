1e783ade7a2197db8c8a8cc6fa8bd179e137e79d
dohye1 <tsc03018@gmail.com>
2023-02-11 12:30:48 +0900

    add new error

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ const Tracing = require("@sentry/tracing");
 
 Sentry.init({
   dsn: "https://bc704cfac9c4493696bce71e2f465929@o4504581152768000.ingest.sentry.io/4504638671159296",
-
+  environment: "Production",
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
+  release: process.env.VERSION,
+});
+
+Sentry.addBreadcrumb({
+  level: "info",
+  category: "URL Endpoints",
+  message: "In the handled function",
 });
 
 const transaction = Sentry.startTransaction({
@@ -25,10 +32,19 @@ function foo() {
   throw new Error("hyewon server test");
 }
 
+function hyewonOcurredError() {
+  throw new Error("this is my new ERRRRRRRORRRRR");
+}
+
 setTimeout(() => {
   try {
-    foo();
+    hyewonOcurredError();
   } catch (e) {
+    Sentry.addBreadcrumb({
+      level: "error",
+      category: "Intended Error",
+      message: "this error ocurred by myself",
+    });
     Sentry.captureException(e);
   } finally {
     transaction.finish();
